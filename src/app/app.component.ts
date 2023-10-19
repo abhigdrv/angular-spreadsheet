151125daf3b27ab916ef5970d9e3fb1cfc07ff51
Abhishek Vishwakarma <52538014+abhigdrv@users.noreply.github.com>
2023-10-19 12:12:01 +0530

    added syncfusion/ej2-angular-spreadsheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-spreadsheet';
+  public data: object[] = [];
+  ngOnInit(): void {
+    this.data = [
+        { OrderID: 10248, CustomerID: 'VINET', EmployeeID: 5, ShipCity: 'Reims' },
+        { OrderID: 10249, CustomerID: 'TOMSP', EmployeeID: 6, ShipCity: 'Münster' },
+        { OrderID: 10250, CustomerID: 'HANAR', EmployeeID: 4, ShipCity: 'Lyon' }
+    ];
+}
 }
